4ff14ea1365486a93e53889ee66e7049d2575781
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-08-13 11:48:55 -0700

    Fix the build for step00

--- step00/src/main.rs
+++ step00/src/main.rs
@@ -1,18 +1,17 @@
 extern crate hyper;
 
-
-use std::env;
-use std::io;
-use std::sync::mpsc;
-use std::time::Duration;
-
-use hyper::client::{Client, Request, Response};
-
+use std::io::Read;
+use hyper::client::Client;
 
 fn main() {
     let client = Client::new();
 
-    let res = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS").send().unwrap();
+    let mut response =
+        client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS")
+              .send().unwrap();
 
-    println!("Response: {}", res.body);
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
